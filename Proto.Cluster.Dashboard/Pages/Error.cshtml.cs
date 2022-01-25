3fabfcecd7e4b3f54a13d3ea6ca95988d3e188f8
Roger Johansson <rogeralsing@gmail.com>
2022-01-25 16:49:53 +0100

    hexagons

--- Proto.Cluster.Dashboard/Pages/Error.cshtml.cs
+++ Proto.Cluster.Dashboard/Pages/Error.cshtml.cs
@@ -8,16 +8,16 @@ namespace Proto.Cluster.Dashboard.Pages;
 [IgnoreAntiforgeryToken]
 public class ErrorModel : PageModel
 {
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-
     private readonly ILogger<ErrorModel> _logger;
 
     public ErrorModel(ILogger<ErrorModel> logger)
     {
         _logger = logger;
     }
+
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
     public void OnGet()
     {
